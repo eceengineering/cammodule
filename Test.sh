f2ed45cf771dbd2c62437505c52f21756cb6c1c4
eceengineering <ozgur.eralp@outlook.com>
2014-12-16 13:59:21 +0000

    Create Test.sh

---
+++ Test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+echo "-------------------------------------------------------------------"
+echo " THIS IS A TEST APPLICATION IMPLEMENTED FOR CAMMODULE " 
+echo " ASSUMPTION:"
+echo "	- Camera able to capture YUV (UYVY) format."
+echo "-------------------------------------------------------------------"
+
+sudo ./armTestApp
